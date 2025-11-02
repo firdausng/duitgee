6af3896b4d281104a7dbc9b900fea72e953e0981
Firdaus Kamaruddin <mail4frenz@gmail.com>
2025-11-03 05:26:08 +0800

    Update project dependencies and environment configuration
    Details:
    - Adjust `.gitignore` to exclude `.env*` and `.idea` files.
    - Update `worker-configuration.d.ts` with new types and bindings.
    - Add Tailwind CSS, Valibot, and other libraries to `package.json`.
    - Refresh `pnpm-lock.yaml` to reflect updated dependencies.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,3 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
         interface Platform {
@@ -7,6 +5,42 @@ declare global {
             cf: CfProperties
             ctx: ExecutionContext
         }
+        interface Api {
+            Bindings: Cloudflare.Env,
+            Variables: RequestIdVariables & {
+                currentSession: AuthSession
+                [PUBLIC_CATEGORIES_KEY]: CategoryData
+            }
+        }
+        interface Locals {
+            error: Error
+            currentSession: AuthSession
+            currentUser: User
+            isVaultLimitReach: boolean
+        }
+
+        interface AuthSession {
+            user:User
+            session:Session
+        }
+
+        interface User {
+            id: string
+            createdAt: Date
+            updatedAt: Date
+            email: string
+            emailVerified: boolean
+            name: string
+            role?: string | null | undefined
+            image?: string | null | undefined
+            banned: boolean | null | undefined,
+            banReason?: string | null | undefined,
+            banExpires?: Date | null | undefined,
+        }
+    }
+
+    namespace Client {
+
     }
 }
 
