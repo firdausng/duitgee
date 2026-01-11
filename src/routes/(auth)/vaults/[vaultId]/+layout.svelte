e95537417b022172f331b7d87f494ef95f1f0f3c
Firdaus Kamaruddin <mail4frenz@gmail.com>
2026-01-11 18:59:41 +0800

    Hide Floating Action Button on the "Create Expense" page to improve usability

--- src/routes/(auth)/vaults/[vaultId]/+layout.svelte
+++ src/routes/(auth)/vaults/[vaultId]/+layout.svelte
@@ -16,6 +16,8 @@
     // Check if we're on the statistics page to adjust button position
     let isStatisticsPage = $derived(page.url.pathname.includes('/statistics'));
 
+    let isCreateExpensePage = $derived(page.url.pathname.includes('/expenses/new'));
+
     let fabItems = new SvelteSet();
     fabItems.add({
         label: 'Add Expense',
@@ -27,26 +29,29 @@
     {@render children?.()}
 </div>
 
-{#each fabItems as fab}
-    <!-- Floating Action Button -->
-    <FloatingActionButton
-            onclick={fab.onClick}
-            class={isStatisticsPage ? 'bottom-[calc(7rem+env(safe-area-inset-bottom))] sm:bottom-[7rem]' : ''}
-    >
-        {#snippet icon()}
-            <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-            >
-                <path
-                        fill-rule="evenodd"
-                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                        clip-rule="evenodd"
-                />
-            </svg>
-        {/snippet}
-        {fab.label}
-    </FloatingActionButton>
-{/each}
+{#if !isCreateExpensePage}
+    {#each fabItems as fab}
+        <!-- Floating Action Button -->
+        <FloatingActionButton
+                onclick={fab.onClick}
+                class={isStatisticsPage ? 'bottom-[calc(7rem+env(safe-area-inset-bottom))] sm:bottom-[7rem]' : ''}
+        >
+            {#snippet icon()}
+                <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-5 h-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                >
+                    <path
+                            fill-rule="evenodd"
+                            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                            clip-rule="evenodd"
+                    />
+                </svg>
+            {/snippet}
+            {fab.label}
+        </FloatingActionButton>
+    {/each}
+{/if}
+
